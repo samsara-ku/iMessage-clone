175586e92624ed6f4ad383d7d275ca4b92de233a
yunhoe <yunhoe@deepnatural.ai>
2020-10-27 17:29:55 +0900

    firebase login fucntion is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import IMessage from './Imessage'
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectUser, login, logout } from './features/userSlice'
 import Login from './Login'
+import { auth } from './firebase'
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        )
+      } else {
+        // user is logged out
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   return <div className="app">{user ? <IMessage /> : <Login />}</div>
 }

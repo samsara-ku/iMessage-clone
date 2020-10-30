9c9e1b249431b19bf3ec423b0216ee1d721c41ee
yunhoe <yunhoe@deepnatural.ai>
2020-10-30 19:13:16 +0900

    Rest of CSS work is done

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -4,6 +4,7 @@ import './SidebarChat.css'
 import { setChat } from './features/chatSlice'
 import { useDispatch } from 'react-redux'
 import db from './firebase'
+import * as timeago from 'timeago.js'
 
 function SidebarChat({ id, chatName }) {
   const dispatch = useDispatch()
@@ -36,7 +37,7 @@ function SidebarChat({ id, chatName }) {
         <h3>{chatName}</h3>
         <p>{chatInfo[0]?.message}</p>
         <small>
-          {new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()}
+          {timeago.format(new Date(chatInfo[0]?.timestamp?.toDate()))}
         </small>
       </div>
     </div>
